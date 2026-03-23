8d186dda8fb639b46245b06b46c90536aa0da3b7
Santatriniaina17 <dilan.santatra2000@gmail.com>
2026-03-23 10:20:07 +0300

    update ML

--- front/src/app/services/prediction.service.ts
+++ front/src/app/services/prediction.service.ts
@@ -7,6 +7,7 @@ import { PredictionResult, SellerType } from '../models/prediction.model';
   providedIn: 'root',
 })
 export class PredictionService {
+  //private apiUrl = 'http://127.0.0.1:8000/api/predict/';
   private apiUrl = 'https://misera-final.onrender.com/api/predict/';
   constructor(private http: HttpClient) {}
   predict(file: File, type: SellerType): Observable<PredictionResult> {
